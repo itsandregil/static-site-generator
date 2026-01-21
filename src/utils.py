3d29dfdd6bc00b73970a5fbd032477a5bc90fb56
Andre Gil <thomasgil913@gmail.com>
2026-01-21 16:25:02 -0500

    feat: full inline markdown elements parsing

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import re
 
 Delimiter = Literal["**", "_", "`"]
 
+
 # TODO: Build support nested inline elements
 def split_node_delimiter(
     old_nodes: list[TextNode],
@@ -15,19 +16,18 @@ def split_node_delimiter(
     for node in old_nodes:
         if node.text_type != TextType.TEXT:
             new_nodes.append(node)
-
-        parts = node.text.split(sep=delimeter)
-        if len(parts) % 2 == 0:
-            raise Exception("Invalid markdown syntax")
-
+            continue
         split_nodes = []
-        for i, part in enumerate(parts):
-            if not part:
+        sections = node.text.split(delimeter)
+        if len(sections) % 2 == 0:
+            raise Exception("Invalid markdown syntax")
+        for i in range(len(sections)):
+            if sections[i] == "":
                 continue
             if i % 2 == 0:
-                split_nodes.append(TextNode(part, TextType.TEXT))
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
             else:
-                split_nodes.append(TextNode(part, text_type))
+                split_nodes.append(TextNode(sections[i], text_type))
         new_nodes.extend(split_nodes)
     return new_nodes
 
@@ -40,9 +40,53 @@ def extract_markdown_links(text: str) -> list[tuple[str, str]]:
     return re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
 
 
-def split_nodes_image(old_nodes: list[TextNode]):
-    pass
+def split_nodes_images(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    for node in old_nodes:
+        images = extract_markdown_images(node.text)
+        if not images:
+            new_nodes.append(node)
+            continue
+        split_nodes = []
+        current_text = node.text
+        for alt_text, url in images:
+            before, after = current_text.split(f"![{alt_text}]({url})", 1)
+            if before:
+                split_nodes.append(TextNode(before, TextType.TEXT))
+            split_nodes.append(TextNode(alt_text, TextType.IMAGE, url))
+            current_text = after
+        if current_text:
+            split_nodes.append(TextNode(current_text, TextType.TEXT))
+        new_nodes.extend(split_nodes)
+    return new_nodes
 
 
 def split_nodes_links(old_nodes: list[TextNode]):
-    pass
+    new_nodes = []
+    for node in old_nodes:
+        links = extract_markdown_links(node.text)
+        if not links:
+            new_nodes.append(node)
+            continue
+        split_nodes = []
+        current_text = node.text
+        for text, url in links:
+            before, after = current_text.split(f"[{text}]({url})", 1)
+            if before:
+                split_nodes.append(TextNode(before, TextType.TEXT))
+            split_nodes.append(TextNode(text, TextType.LINK, url))
+            current_text = after
+        if current_text:
+            split_nodes.append(TextNode(current_text, TextType.TEXT))
+        new_nodes.extend(split_nodes)
+    return new_nodes
+
+
+def text_to_textnodes(text: str) -> list[TextNode]:
+    node = TextNode(text, TextType.TEXT)
+    new_nodes = split_node_delimiter([node], "**", TextType.BOLD)
+    new_nodes = split_node_delimiter(new_nodes, "_", TextType.ITALIC)
+    new_nodes = split_node_delimiter(new_nodes, "`", TextType.CODE)
+    new_nodes = split_nodes_images(new_nodes)
+    new_nodes = split_nodes_links(new_nodes)
+    return new_nodes
--- test/test_delimiter_split.py
+++ test/test_delimiter_split.py
@@ -1,10 +1,10 @@
 import unittest
-from src.utils import split_node_delimiter
+from src.utils import split_node_delimiter, split_nodes_images, text_to_textnodes
 from src.text_node import TextNode, TextType
 
 
-class TestSplitOnDelimeter(unittest.TestCase):
-    def test_split_node(self):
+class TestInlineMarkdownParsing(unittest.TestCase):
+    def test_split_nodes_with_delimiter(self):
         node = TextNode("Hello **world** friend", TextType.TEXT)
         new_nodes = split_node_delimiter([node], "**", TextType.BOLD)
         self.assertListEqual(
@@ -13,5 +13,44 @@ class TestSplitOnDelimeter(unittest.TestCase):
                 TextNode("Hello ", TextType.TEXT),
                 TextNode("world", TextType.BOLD),
                 TextNode(" friend", TextType.TEXT),
+            ],
+        )
+
+    def test_split_nodes_with_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_images([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_markdown_to_textnodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            nodes,
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode(
+                    "obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"
+                ),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
             ],
         )
