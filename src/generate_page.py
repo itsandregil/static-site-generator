cdfa4098235e79a028abf3c91e909c653a26e5e9
Andre Gil <thomasgil913@gmail.com>
2026-01-25 16:16:25 -0500

    change page generation for Github Pages compatibility

--- src/generate_page.py
+++ src/generate_page.py
@@ -12,7 +12,20 @@ def extract_markdown_title(markdown: str) -> str:
     raise Exception("Markdown content must have a title")
 
 
-def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+def replace_content(
+    template_content: str, *, title: str, html: str, base_path: str
+) -> str:
+    return (
+        template_content.replace("{{ Title }}", title)
+        .replace("{{ Content }}", html)
+        .replace('href="/', f"href={base_path}")
+        .replace('src="/', f"src={base_path}")
+    )
+
+
+def generate_page(
+    from_path: str, template_path: str, dest_path: str, base_path: str
+) -> None:
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     with open(from_path, "r") as file:
@@ -22,8 +35,12 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
 
     page_title = extract_markdown_title(markdown_content)
     html = markdown_to_html_node(markdown_content).to_html()
-    html_content = template_content.replace("{{ Title }}", page_title)
-    html_content = template_content.replace("{{ Content }}", html)
+    html_content = replace_content(
+        template_content,
+        title=page_title,
+        html=html,
+        base_path=base_path,
+    )
 
     if not os.path.exists(dest_path):
         os.makedirs(os.path.dirname(dest_path), exist_ok=True)
@@ -33,9 +50,7 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
 
 
 def generate_pages_recursive(
-    dir_content_path: str,
-    template_path: str,
-    dest_dir_path: str,
+    dir_content_path: str, template_path: str, dest_dir_path: str, base_path: str
 ) -> None:
     for file in os.listdir(dir_content_path):
         src_path = os.path.join(dir_content_path, file)
@@ -44,6 +59,6 @@ def generate_pages_recursive(
             raise Exception(f"Content must have only markdown files: {src_path}")
         if os.path.isfile(src_path):
             dest_path = dest_path.replace(".md", ".html")
-            generate_page(src_path, template_path, dest_path)
+            generate_page(src_path, template_path, dest_path, base_path)
         else:
-            generate_pages_recursive(src_path, template_path, dest_path)
+            generate_pages_recursive(src_path, template_path, dest_path, base_path)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 
 from src.copystatic import copy_files_to_dir
 from src.generate_page import generate_pages_recursive
@@ -7,8 +8,11 @@ logging.basicConfig(level=logging.INFO)
 
 
 def main():
-    copy_files_to_dir("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    base_path = "/"
+    if len(sys.argv) > 1:
+        base_path = sys.argv[1]
+    copy_files_to_dir("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", base_path)
 
 
 if __name__ == "__main__":
